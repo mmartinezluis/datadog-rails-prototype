290b02359d9ccd47dac1da4d1acbc70364edbc85
Luis Martinez <luis_mmartinez@live.com>
2023-04-17 09:54:46 -0400

    add ddtrace gem for using datadog; add datadog initializer file

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,7 +1,9 @@
 Datadog.configure do |c|
   # This will activate auto-instrumentation for Rails
-#   c.use :rails, {'analytics_enabled': true, 'service_name': 'store-frontend', 'cache_service': 'store-frontend-cache', 'database_service': 'store-frontend-sqlite'}
-  c.use :rails, {'analytics_enabled': true, 'service_name': 'fullstack-app'}
+  c.tracing.instrument :rails, {'analytics_enabled': true, 'service_name': 'fullstack-app'}
   # Make sure requests are also instrumented
-  c.use :http, {'analytics_enabled': true, 'service_name': 'fullstack-app'}
+  c.tracing.instrument :http, {'analytics_enabled': true, 'service_name': 'fullstack-app'}
 end
+
+
+#   c.use :rails, {'analytics_enabled': true, 'service_name': 'store-frontend', 'cache_service': 'store-frontend-cache', 'database_service': 'store-frontend-sqlite'}
